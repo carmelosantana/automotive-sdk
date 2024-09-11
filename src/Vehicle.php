e339c97ac417ab414b794f4b5260558b72046493
Carmelo Santana <me@carmelosantana.com>
2024-09-11 16:28:53 -0400

    Update Vehicle labels

--- src/Vehicle.php
+++ src/Vehicle.php
@@ -212,11 +212,40 @@ class Vehicle
     // Register post type
     public function registerPostType()
     {
+        // labels to change all "Post(s)" to "Vehicle(s)"
+        $labels = [
+            'name' => __('Vehicles'),
+            'singular_name' => __('Vehicle'),
+            'add_new' => __('Add Vehicle'),
+            'add_new_item' => __('Add New Vehicle'),
+            'edit_item' => __('Edit Vehicle'),
+            'new_item' => __('New Vehicle'),
+            'view_item' => __('View Vehicle'),
+            'view_items' => __('View Vehicles'),
+            'search_items' => __('Search Vehicles'),
+            'not_found' => __('No Vehicles found'),
+            'not_found_in_trash' => __('No Vehicles found in Trash'),
+            'all_items' => __('All Vehicles'),
+            'archives' => __('Vehicle Archives'),
+            'attributes' => __('Vehicle Attributes'),
+            'insert_into_item' => __('Insert into Vehicle'),
+            'uploaded_to_this_item' => __('Uploaded to this Vehicle'),
+            'featured_image' => __('Featured Image'),
+            'set_featured_image' => __('Set featured image'),
+            'remove_featured_image' => __('Remove featured image'),
+            'use_featured_image' => __('Use as featured image'),
+            'filter_items_list' => __('Filter Vehicles list'),
+            'items_list_navigation' => __('Vehicles list navigation'),
+            'items_list' => __('Vehicles list'),
+            'item_published' => __('Vehicle published.'),
+            'item_published_privately' => __('Vehicle published privately.'),
+            'item_reverted_to_draft' => __('Vehicle reverted to draft.'),
+            'item_scheduled' => __('Vehicle scheduled.'),
+            'item_updated' => __('Vehicle updated.'),
+        ];
+
         register_post_type('vehicle', [
-            'labels' => [
-                'name' => __('Vehicles'),
-                'singular_name' => __('Vehicle'),
-            ],
+            'labels' => $labels,
             'public' => true,
             'has_archive' => true,
             'rewrite' => ['slug' => 'vehicles'],
